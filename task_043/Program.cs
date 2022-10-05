e8c38eb9b3dda611bb57f231c6d32bc94a281697
goodie <vorkuta@inbox.ru>
2022-10-06 01:25:00 +0300

    task_043 completed

---
+++ task_043/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+Write("Введите b1, k1, b2, k2 (через пробел): ");
+
+string[] input = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+float[] arg = new float[4];
+for (int i = 0; i < input.Length; i++) arg[i] = float.Parse(input[i]);
+var intersection = LineIntersection(arg[0], arg[1], arg[2], arg[3]);
+
+WriteLine($"Точка пересечения (x, y): {(intersection != null ? string.Join(", ", intersection) : "отсутствует (или \u221E)")}");
+
+float[] LineIntersection(float b1, float k1, float b2, float k2)
+{
+    if (k1 == k2) return null!;
+
+    float[] result = new float[2];
+
+    result[0] = (b2 - b1) / (k1 - k2);
+    result[1] = k1 * result[0] + b1;
+
+    return result;
+}
