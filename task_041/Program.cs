62dea06d3578bc1d9ee7064e960a73162ae6bae7
goodie <vorkuta@inbox.ru>
2022-10-05 22:48:50 +0300

    task_041 completed

---
+++ task_041/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Write("Введите ряд чисел через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+WriteLine($"Количество положительных чисел = {ArrayZeroSum(parameters)}");
+
+int ArrayZeroSum(string[] inArray)
+{
+    int result = 0;
+    foreach (string element in inArray)
+    {
+        if (int.Parse(element) > 0) result++;
+    }
+    return result;
+}
