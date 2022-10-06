e3da79e779b9b5ed40b5cde5488acf21e572dae3
Pavel Yastremsky <goodieart@gmail.com>
2022-10-06 12:56:07 +0300

    solution added

---
+++ lib/helper.cs
@@ -0,0 +1,28 @@
+﻿namespace Lib;
+public class Helper
+{
+    public static int ArrayZeroSum(string[] inArray)
+    {
+        int result = 0;
+        foreach (string element in inArray)
+        {
+            if (int.Parse(element) > 0) result++;
+        }
+        return result;
+    }
+    public static float[] LineIntersection(float b1, float k1, float b2, float k2)
+    {
+        if (k1 == k2) return null!;
+
+        float[] result = new float[2];
+
+        result[0] = (b2 - b1) / (k1 - k2);
+        result[1] = k1 * result[0] + b1;
+
+        return result;
+    }
+    public static string[] GetUserInput(string separator = " ")
+    {
+        return ReadLine()!.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+    }
+}
--- task_041/Program.cs
+++ task_041/Program.cs
@@ -1,15 +1,6 @@
-﻿using static System.Console;
+﻿using Lib;
+using static System.Console;
 
 Write("Введите ряд чисел через пробел: ");
-string[] input = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-WriteLine($"Количество положительных чисел = {ArrayZeroSum(input)}");
-
-int ArrayZeroSum(string[] inArray)
-{
-    int result = 0;
-    foreach (string element in inArray)
-    {
-        if (int.Parse(element) > 0) result++;
-    }
-    return result;
-}
+string[] input = Helper.GetUserInput();
+WriteLine($"Количество положительных чисел = {Helper.ArrayZeroSum(input)}");
